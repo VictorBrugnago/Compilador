51b199149bf3ce3a4c8dd84bb7d46457b73e6bf1
Victor Brugnago <jv7@hotmail.com.br>
2019-09-05 00:51:15 -0400

    .

--- AnalisadorLexico/AnalisadorLexico.py
+++ AnalisadorLexico/AnalisadorLexico.py
@@ -8,6 +8,7 @@ finish_state_dict = {}
 transition_dict = defaultdict(list)
 line_finish_state = 0
 index = 0
+line_counter = 0
 word = 'startCodeE'
 csv_result = 'result.csv'
 buffer_character = ''
@@ -15,21 +16,25 @@ buffer_character = ''
 is_Text = False
 
 
-def transition(state, char):
+def transition(states, char):
     try:
         for line in token_file:
-            print('State: ', state)
-            print('Char: ', char)
-            if state + ', ' + char in line:
+            if states is None:
+                error = True
+            elif states + ', ' + char in line:
                 print('Return: ', line.split(', ')[2].rstrip())
                 return line.split(', ')[2].rstrip()
+        return False
         # return transition_dict[state, char]
     except KeyError:
         return False
 
 
 def variable(token_buffer):
+    print('Entrou no variables')
     for char in token_buffer:
+        print('\n\nvariables char: ', char)
+        print('\n\n')
         if transition('q63', char) is not False:
             return True
         else:
@@ -42,8 +47,8 @@ def write_result_csv(token, lexeme, position):
     :param lexeme:
     :param position: position of the first letter of the word -> Line:column
     """
-    with open('csv_test.csv', 'a+') as csv_result:
-        csv_result_writer = csv.writer(csv_result, delimiter=',', dialect='excel', lineterminator='\n')
+    with open('csv_test.csv', 'w+') as csv_results:
+        csv_result_writer = csv.writer(csv_results, delimiter=',', dialect='excel', lineterminator='\n')
         csv_result_writer.writerow([token, lexeme, position])
 
 
@@ -62,6 +67,9 @@ with open('Tokens.config', newline='') as tokens:
     for line in buff_reader_tokens:
         if line[0][0] != '#':
             token_list.append(line)
+
+with open('code.foo', 'r') as file:
+    source_code = file.readlines()
 
 
 # Loading Initial State variable
@@ -87,36 +95,108 @@ with open('tokens.config', 'r') as file:
 # print('Finish State(s): ', finish_state_dict)
 # print('Transitions: ', transition_dict)
 
-state = initial_state
-while index < len(word):
-    character = str(word[index])
+for line in source_code:
+    state = initial_state
+    buffer_character = ''
+    line_counter += 1
+    column_counter = 0
 
-    if character == '"' and is_Text is False:
-        is_Text = True
-    elif character == '"' and is_Text is True:
-        is_Text = False
-        if state == 'q60':
-            state = 'q61'
+    is_Text = False
 
-    if is_Text is True:
-        pass
-    else:
-        state = transition(state, character)
-        buffer_character = buffer_character + character  # Concatenate each char to create the complete word
-        print('Buffer: ', buffer_character)
+    if line[len(line) - 1] == '\n':  # To ignore \n
+        line = line.rstrip()
 
-        if transition(state, character) is False:  # if the char doesn't have state
-            if state in finish_state_dict.keys():  # if the actual state is a finish state
-                write_result_csv(buffer_character, state, '0:0')
-                state = 'q0'
-                buffer_character = ''
-            elif variable(buffer_character) is True:
-                state = 'q63'
-                buffer_character = ''
-            else:
-                print('Caractere {} não esperado'.format(character))
-                state = 'q0'
-                buffer_character = ''
+    while column_counter < len(line):
+        character = line[column_counter]
+        # buffer_character = buffer_character + character  # Concatenate each char to create the complete word
+        print('Character: {}  | Column: {}  |  Line: {}'.format(character, column_counter, line_counter))
+
+        if character == '"' and is_Text is False:
+            is_Text = True
+        elif character == '"' and is_Text is True:
+            is_Text = False
+            if state == 'q60':
+                state = 'q61'
+
+        if state is initial_state:
+            new_column_word = column_counter
+        if is_Text is True:
+            pass
+        else:
+            if character != ' ':
+                print('Entrou no ELSE...')
+                print('Actual State: ', state)
+                state = transition(state, character)
+                buffer_character = buffer_character + character  # Concatenate each char to create the complete word
+                print('Buffer: ', buffer_character)
+
+                if column_counter < len(line):
+                    if transition(state, character) is False:  # if the char doesn't have state
+                        print('Entrou no primeiro IF')
+                        if state in finish_state_dict.keys():  # if the actual state is a finish state
+                            print('Entrou no state IF')
+                            write_result_csv(buffer_character, finish_state_dict.get(state),
+                                             str(column_counter) + ':' + str(line_counter))
+                            state = initial_state
+                            buffer_character = ''
+                        elif variable(buffer_character) is True:
+                            print('entrou no variable if')
+                            write_result_csv(buffer_character, finish_state_dict.get(state),
+                                             str(column_counter) + ':' + str(line_counter))
+                            state = 'q63'
+                            buffer_character = ''
+                            column_counter = new_column_word - 1
+                        else:
+                            print('Caractere {} não esperado'.format(character))
+                            state = initial_state
+                            buffer_character = ''
+                if column_counter == len(line) and buffer_character == len(line):
+                    if variable(buffer_character) is False:
+                        print('Caractere {} não esperado  | {}:{}'.format(character, new_column_word, line_counter))
+                    else:
+                        write_result_csv(buffer_character, finish_state_dict.get(state),
+                                         str(column_counter) + ':' + str(line_counter))
+                        state = initial_state
+                        buffer_character = ''
+
+        column_counter += 1
+
+# state = initial_state
+# while index < len(word):
+#     character = str(word[index])
+#
+#     if character == '"' and is_Text is False:
+#         is_Text = True
+#     elif character == '"' and is_Text is True:
+#         is_Text = False
+#         if state == 'q60':
+#             state = 'q61'
+#
+#     if is_Text is True:
+#         pass
+#     else:
+#         print('Entrou no ELSE...')
+#         state = transition(state, character)
+#         buffer_character = buffer_character + character  # Concatenate each char to create the complete word
+#         print('Buffer: ', buffer_character)
+#
+#         if transition(state, character) is False:  # if the char doesn't have state
+#             print('Entrou no primeiro IF')
+#             if state in finish_state_dict.keys():  # if the actual state is a finish state
+#                 print('Entrou no state IF')
+#                 write_result_csv(buffer_character, finish_state_dict.get(state), '0:0')
+#                 state = 'q0'
+#                 buffer_character = ''
+#             elif variable(buffer_character) is True:
+#                 print('entrou no variable if')
+#                 #write_result_csv(buffer_character, finish_state_dict.get(state), '0:0')
+#                 state = 'q63'
+#                 buffer_character = ''
+#             else:
+#                 print('Caractere {} não esperado'.format(character))
+#                 state = 'q0'
+#                 buffer_character = ''
+#     index += 1
 
 # state = initial_state
 # while index < len(word):
@@ -133,8 +213,10 @@ while index < len(word):
 #     #     error = True
 #     index += 1
 
-print('\nResults')
-if state in finish_state_dict.keys() and error is False:
-    print(word, ' accept')
-else:
-    print(word, ' not accept')
+# print('\nResults')
+# if state in finish_state_dict.keys() and error is False:
+#     print(word, ' accept')
+#     print('State: ', state)
+#     print('Lexeme: ', finish_state_dict.get(state))
+# else:
+#     print(word, ' not accept')
