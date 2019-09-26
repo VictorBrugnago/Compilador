636ae09427de49355ba0784595fb9545f0cff688
Victor Brugnago <jv7@hotmail.com.br>
2019-09-26 16:46:42 -0400

    Changed Stack Library.

--- Scripts/AnalisadorSintatico/AnalisadorSintatico.py
+++ Scripts/AnalisadorSintatico/AnalisadorSintatico.py
@@ -1,21 +1,25 @@
-from queue import LifoQueue
+from collections import deque
 import csv
 
 # Queue & Stack
 queue = []
-stack = LifoQueue()
+stack = deque()
 
 # Open list of Tokens and inserting '$' at the beginning of the list
 with open('Tokens.txt', 'r', encoding='utf-8', newline='') as token_file:
     tokens_readed = csv.reader(token_file, delimiter=',', skipinitialspace=True)
-    queue.append('$')
     for line in (list(tokens_readed)[1:]):  # Starts from line 1
         queue.append(line)
+    queue.append('$')
 
 # Inserting '$' in the end of stack
-stack.put('$')
-
+stack.append('$')
 
 print('Queue')
+for i in queue:
+    print(i)
+
+queue.pop(0)
+print('\n\n')
 for i in queue:
     print(i)
