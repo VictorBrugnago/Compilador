161130252601bfa7094607df3dd2b74b7cfbd57f
Victor Brugnago <jv7@hotmail.com.br>
2019-09-28 04:24:54 -0400

    Improved SyntaxError message; Improved Sentence Matching message.

--- Scripts/AnalisadorSintatico/AnalisadorSintatico.py
+++ Scripts/AnalisadorSintatico/AnalisadorSintatico.py
@@ -1,6 +1,11 @@
 from collections import deque
+from colorama import Fore
+import colorama
 import csv
 import sys
+
+# Set colorama library init
+colorama.init(autoreset=True)
 
 # Lists
 syntactic_list = []
@@ -16,7 +21,7 @@ queue = []
 stack = deque()
 
 # Count
-cont = 0
+location_error = 0
 
 
 def transition(non_terminal_word, terminal_word):
@@ -26,7 +31,6 @@ def transition(non_terminal_word, terminal_word):
         print('(transition def) Grammar id: ', syntactic_state_dict[non_terminal_word, terminal_word])
         return syntactic_state_dict[non_terminal_word, terminal_word]
     else:
-        print('Error')
         return 'error'
 
 
@@ -57,39 +61,26 @@ for line_syntactic in range(len(syntactic_list)):
 
 # Open list of Tokens and inserting '$' at the beginning of the list
 with open('TokensResultLexical.txt', 'r', encoding='utf-8', newline='') as token_file:
+    # Creating a list of tokens to using as referencial to syntax erros
+    tokens_list = token_file.readlines()
+    del tokens_list[0]
+    token_file.seek(0)
+
+    # Loading the queue
     tokens_readed = csv.reader(token_file, delimiter=',', skipinitialspace=True)
     for line in (list(tokens_readed)[1:]):  # Starts from line 1
         queue.append(line[0])
-
-with open('TokensResultLexical.txt', 'r', encoding='utf-8', newline='') as f:
-    lista_tokens = f.readlines()
-
-del lista_tokens[0]
-queue.append('$')
+    queue.append('$')
 
 # Inserting '$' in the end of stack and inserting the Sentential symbol
 stack.append('$')
 stack.append('<PROGRAM>')
 
-# for keys,values in syntactic_state_dict.items():
-#     print(keys,values)
-
-# print(syntactic_state_dict['<VARIABLE_LIST>', 'int'])
-# print(grammar_state_dict.get('0'))
-# print(len(grammar_state_dict.get('0')))
-# print(type(grammar_state_dict.get('0')))
-# print(len(grammar_state_dict.get('0')) - 1)
-# print(grammar_state_dict.get('0')[2])
-# print(grammar_state_dict.get('0')[2] != 'tk_abre_bloco')
-
-
 while queue and stack:
     print('\nUnstacking...')
     non_terminal_symb = stack[-1]
     print('NonTerminal symbol on top of stack: ', non_terminal_symb)
-    print('Testing: ', queue[0])
 
-    # if non_terminal_symb.isupper() is True:
     if non_terminal_symb.isupper():
         print('\nDequeuing...')
         terminal_symb = queue[0]
@@ -97,9 +88,11 @@ while queue and stack:
 
         grammar = transition(non_terminal_symb, terminal_symb)  # Testing
         if grammar == 'error':
-            # print('Error')
-            print(f"\nERRO: {lista_tokens[cont].split(', ')[1]} linha:{lista_tokens[cont].split(', ')[2]} coluna:{lista_tokens[cont].split(', ')[3]}")
-            break
+            print(Fore.RED + 'SyntaxError: invalid syntax \'{}\' in line {}'.format(
+                tokens_list[location_error].split(', ')[1],
+                tokens_list[location_error].split(', ')[2]
+            ))
+            sys.exit()
         stack.pop()
 
         print('\nGrammar id: ', grammar)
@@ -111,25 +104,13 @@ while queue and stack:
                 stack.append(grammar_state_dict.get(grammar)[grammar_count])
             grammar_count -= 1
     elif stack[-1] == queue[0]:
-        print(f'#{non_terminal_symb} = {queue[0]}#')
+        print('Sentence recognized: {} -> {}'.format(non_terminal_symb, queue[0]))
         del queue[0]
-        cont += 1
+        location_error += 1
         stack.pop()
 
 if not queue and not stack:
     print('\n\nSyntactic Analyzer completed!')
 else:
-    print("Error --> Stack or Queue ar not empty")
+    print("Error --> Stack or Queue are not empty")
     sys.exit()
-
-# print('\n\nSyntactic Analyzer completed!') if not fila and pilha.isEmpty() else print("Erro pilha ou fila nao estao vazias")
-
-
-# print('Queue')
-# for i in queue:
-#     print(i)
-#
-# queue.pop(0)
-# print('\n\n')
-# for i in queue:
-#     print(i)
