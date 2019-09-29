571e83d0867dfd096f7893d8bacbb53bd210633e
Victor Brugnago <jv7@hotmail.com.br>
2019-09-28 22:02:15 -0400

    Added Syntactic Analyzer function; Added more parameters.

--- FooCompiler/FooCompiler.py
+++ FooCompiler/FooCompiler.py
@@ -1,4 +1,5 @@
 from Analyzers.LexicalAnalyzer.AnalisadorLexico import lexical_analyser
+from Analyzers.SyntacticAnalyzer.AnalisadorSintatico import syntactic_analyzer
 from colorama import Fore
 import argparse
 import colorama
@@ -7,8 +8,10 @@ import sys
 
 source_code_name = ''
 
-val_flag = False
+vla_flag = False
+vsa_flag = False
 lt_flag = False
+ls_flag = False
 
 # Set colorama library init
 colorama.init(autoreset=True)
@@ -19,8 +22,8 @@ parameters = sys.argv[1:]
 # Checking Parameters
 if not parameters:
     print('No parameters detected!')
-    print('Type -> python3 AnalisadorLexico.py [filename].foo [parameters]')
-    print('For help, type -> python3 AnalisadorLexico.py -h')
+    print('Type -> python3 FooCompiler.py [filename].foo [parameters]')
+    print('For help, type -> python3 FooCompiler.py -h')
     sys.exit()
 else:
     if parameters[0] == '-h':
@@ -31,9 +34,9 @@ else:
         print('Example: \n  python3 FooCompiler.py [FILENAME].foo -lt -BR')
         print('\n\nAvailable parameters:')
         print('  -lt\tGenerates a listing of the detected tokens, the result is shown in the terminal')
-        print('  -vc\tDisplays a detailed output of the script')
-        print('  -val\tDisplays a detailed output of the script')
-        print('  -vas\tDisplays a detailed output of the script')
+        print('  -tudo\tDisplays a detailed output of the compiler')
+        print('  -vla\tDisplays a detailed output of the lexicon analyzer')
+        print('  -vsa\tDisplays a detailed output of the syntactic analyzer')
         print('  -BR \tIt chooses the language of the outputs for Brazilian Portuguese, by default is English. -BR -> '
               'Brazilian Portuguese')
         sys.exit()
@@ -42,26 +45,38 @@ else:
         for param in parameters[1:]:
             if param == '-lt':
                 lt_flag = True
+            elif param == '-ls':
+                ls_flag = True
+            elif param == '-tudo':
+                logging.basicConfig(format='%(message)s', level=logging.DEBUG)
+                vla_flag = True
+                vsa_flag = True
+            elif param == '-vla':
+                vla_flag = True
+            elif param == '-vsa':
+                vsa_flag = True
             elif param == '-BR':
                 langBR = True
                 # br = gettext.translation('base', localedir='locales', languages=['pt'])
                 # br.install()
                 # _ = br.gettext
-            elif param == '-vc':
-                logging.basicConfig(format='%(message)s', level=logging.DEBUG)
-            elif param == '-val':
-                val_flag = True
-
     elif not str(parameters[0]).endswith('.foo'):
         print('\n' + Fore.RED + 'Nonexistent file')
         print(Fore.RED + 'File \"{}\" Not found!'.format(str(parameters[0])))
         sys.exit()
 
 print('Performing lexical analysis...')
-result = lexical_analyser(source_code_name, val=val_flag)
+result_lexicon = lexical_analyser(source_code_name, vla=vla_flag)
 print('DONE!')
 
 if lt_flag is True:
     print("List of detected Tokens\n")
-    for i in result:
+    for i in result_lexicon:
         print(i.split(',')[0].center(24), i.split(',')[1].center(24), i.split(',')[2].center(8), i.split(',')[3])
+
+print('\nPerforming syntactic analysis...')
+result_syntactic = syntactic_analyzer(result_lexicon, vsa=vsa_flag)
+
+if ls_flag is True:
+    for i in result_syntactic:
+        print(i)
