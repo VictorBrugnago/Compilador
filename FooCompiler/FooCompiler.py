39db442a8ff1afee19794edb8a1cf4e2bbc17fd8
Victor Brugnago <jv7@hotmail.com.br>
2019-10-01 14:17:12 -0400

    Added new parameters; Renamed kwargs 'vla', 'vsa' and 'vsma' to 'vlex', 'vsyn' and 'vsem'; Added an if to check if log folder exists; Removed unused import.

--- FooCompiler/FooCompiler.py
+++ FooCompiler/FooCompiler.py
@@ -2,18 +2,22 @@ from Analyzers.LexicalAnalyzer.AnalisadorLexico import lexical_analyser
 from Analyzers.SyntacticAnalyzer.AnalisadorSintatico import syntactic_analyzer
 from Analyzers.SemanticAnalyzer.AnalisadorSemantico import semantic_analyzer
 from colorama import Fore
-import argparse
 import colorama
 import logging
 import sys
+import os
 
-source_code_name = ''
-
-vla_flag = False
-vsa_flag = False
-vsma_flag = False
+# Flags
+vlex_flag = False
+vsyn_flag = False
+vsem_flag = False
+all_flag = False
 lt_flag = False
+lp_flag = False
 ls_flag = False
+
+# Source Code
+source_code_name = ''
 
 # Set colorama library init
 colorama.init(autoreset=True)
@@ -36,10 +40,12 @@ else:
         print('Example: \n  python3 FooCompiler.py [FILENAME].foo -lt -BR')
         print('\n\nAvailable parameters:')
         print('  -lt\tGenerates a listing of the detected tokens, the result is shown in the terminal')
+        print('  -lp\tGenerates a listing of the productions performed, the result is shown in the terminal')
+        print('  -ls\tShows all steps performed on detected variables, the result is shown in the terminal')
         print('  -tudo\tDisplays a detailed output of the compiler')
-        print('  -vla\tDisplays a detailed output of the lexicon analyzer')
-        print('  -vsa\tDisplays a detailed output of the syntactic analyzer')
-        print('  -vsma\tDisplays a detailed output of the syntactic analyzer')
+        print('  -vlex\tDisplays a detailed output of the lexicon analyzer')
+        print('  -vsyn\tDisplays a detailed output of the syntactic analyzer')
+        print('  -vsem\tDisplays a detailed output of the syntactic analyzer')
         print('  -BR \tIt chooses the language of the outputs for Brazilian Portuguese, by default is English. -BR -> '
               'Brazilian Portuguese')
         sys.exit()
@@ -48,19 +54,18 @@ else:
         for param in parameters[1:]:
             if param == '-lt':
                 lt_flag = True
+            elif param == '-lp':
+                lp_flag = True
             elif param == '-ls':
                 ls_flag = True
             elif param == '-tudo':
-                logging.basicConfig(format='%(message)s', level=logging.DEBUG)
-                vla_flag = True
-                vsa_flag = True
-                vsma_flag = True
-            elif param == '-vla':
-                vla_flag = True
-            elif param == '-vsa':
-                vsa_flag = True
-            elif param == '-vsma':
-                vsma_flag = True
+                all_flag = True
+            elif param == '-vlex':
+                vlex_flag = True
+            elif param == '-vsyn':
+                vsyn_flag = True
+            elif param == '-vsem':
+                vsem_flag = True
             elif param == '-BR':
                 langBR = True
                 # br = gettext.translation('base', localedir='locales', languages=['pt'])
@@ -71,20 +76,28 @@ else:
         print(Fore.RED + 'File \"{}\" Not found!'.format(str(parameters[0])))
         sys.exit()
 
+# Creating a log folder
+if not os.path.isdir('logs'):
+    os.makedirs('logs')
+
 print('Performing lexical analysis...')
-result_lexicon = lexical_analyser(source_code_name, vla=vla_flag)
-print('DONE!')
+result_lexicon = lexical_analyser(source_code_name, vlex=vlex_flag, vall=all_flag)
 
 if lt_flag is True:
     print("List of detected Tokens\n")
     for i in result_lexicon:
         print(i.split(',')[0].center(24), i.split(',')[1].center(24), i.split(',')[2].center(8), i.split(',')[3])
 
-print('\nPerforming syntactic analysis...')
-result_syntactic = syntactic_analyzer(result_lexicon, vsa=vsa_flag)
+print('\n\nPerforming syntactic analysis...')
+result_syntactic = syntactic_analyzer(result_lexicon, vsyn=vsyn_flag, vall=all_flag)
 
-print('\nperforming semantic analysis...')
-result_semantic = semantic_analyzer(result_lexicon, vsma=vsma_flag)
+if lp_flag is True:
+    print('List of production rules carried out\n')
+    for i in result_syntactic:
+        print(i)
+
+print('\n\nPerforming semantic analysis...')
+result_semantic = semantic_analyzer(result_lexicon, vsem=vsem_flag, vall=all_flag)
 
 if ls_flag is True:
     for i in result_syntactic:
